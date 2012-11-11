98acd50a1d78a269d007f3e1e53e3130d084a6e6
JRoTheGeek <jvrosenman@gmail.com>
2012-11-11 18:31:59 -0500

    Checkpoint completed.

--- wireview.c
+++ wireview.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <pcap.h>
+#include <stdlib.h>
+#include <pcap/pcap.h>
+#define MAX_SIZE 102400 //100KB should be enough.
+
+char* openFile(char* path, char data[MAX_SIZE]);
+void printCap();
 
 int main(int argc, char** argv) {
-
+	char data[MAX_SIZE];
+	char* path = argv[1];
+	pcap_t *cap;
+	cap = pcap_open_offline(path, NULL);
+	pcap_loop(cap, -1, printCap, NULL);
 	return 0;
+}
+
+void printCap(u_char *args, const struct pcap_pkthdr *header, const u_char *pkt) {
+	struct timeval pkt_time = header->ts;
+	printf("Received packet at time %ld    %ld.\n", pkt_time.tv_sec, pkt_time.tv_usec);
 }
