faf1cc44d4446c52df96837472debef6a7f7aabd
Louis Fogel <lafogel@wpi.edu>
2012-11-18 23:23:53 -0500

    IP and ARP packets are now identified

--- wireview.cpp
+++ wireview.cpp
@@ -5,23 +5,25 @@
 #include <time.h>
 #include <pcap.h>
 //internet packet utilities
-#include <netinet/ether.h>
+#include <netinet/if_ether.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
 //network packet utilities
 #include <net/if_arp.h>
+#include <net/ethernet.h>
 //constants
 #define MAX_SIZE 102400 //100KB should be enough.
 #define ADDR_LEN 128
 char* openFile(char* path, char data[MAX_SIZE]);
 void printCap(u_char *args, const struct pcap_pkthdr *header, const u_char *pkt);
 
-int numpackets = 0; 
-class node {
+int numpackets = 0;
+ 
+typedef struct mynode{
     //TODO determine what size this buffer should actually be
     char data[ADDR_LEN];
-    node *next, *last;
-};
+    struct mynode *next, *last;
+} node;
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
@@ -66,6 +68,13 @@ void printCap(u_char *args, const struct pcap_pkthdr *header, const u_char *pkt)
 		#ifdef VULGAR
 			printf("You fucker.\n");
 		#endif
+	}
+	struct ether_header* ethernet = (struct ether_header *)pkt;
+	if(ntohs(ethernet->ether_type)==ETHERTYPE_IP){
+	    printf("This is a fucking IP packet.\n");
+	}
+	else if(ntohs(ethernet->ether_type)==ETHERTYPE_ARP) {
+	    printf("This is a fucking ARP packet.\n");
 	}
 	printf("Received packet at time %ld    %ld.\n", pkt_time.tv_sec, pkt_time.tv_usec);
 	numpackets++;
